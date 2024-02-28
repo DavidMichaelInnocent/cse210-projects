12d1405fc42aecc99676e7fe2d1c17dd662e029d
DavidMichaelInnocent <ldsholder3@gmail.com>
2024-02-28 14:11:16 +0100

    new

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,41 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+        {
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+        
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else 
+        {
+            letter = "F";
+        }
+        Console.WriteLine($"Your later grade is: {letter}");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation you passed the councel");
+        }
+        else
+        {
+            Console.WriteLine("You don't pass it.");
+        }
     }
 }
