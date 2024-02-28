5aab1c330c4dbb42e261df4042b4c16b89320628
DavidMichaelInnocent <ldsholder3@gmail.com>
2024-02-28 21:27:34 +0100

    today

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int answer = int.Parse(Console.ReadLine());
+
+        Random radogener = new Random();
+        int magnum = radogener.Next(1, 101);
+
+        int guest = -1;
+
+        while (guest != magnum)
+        {
+            Console.Write("What is your guess number? ");
+            guest = int.Parse(Console.ReadLine());
+
+            if (magnum > guest)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if(magnum < guest)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("Congrats, You guess it!");
+            }
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int getnumber = -1;
+        while (getnumber != 0)
+        {
+            Console.Write("Please Enter a list of  numbers: type 0 when finished. ");
+
+            string reponse = Console.ReadLine();
+            getnumber = int.Parse(reponse);
+
+            if ( getnumber != 0)
+            {
+                numbers.Add(getnumber);
+            }
+        }
+        int summ = 0;
+        foreach (int number in numbers)
+        {
+            summ += number;
+ 
+        }
+        Console.WriteLine($"The sum is: {summ}");
+
+        float avernumber = ((float)summ) / numbers.Count;
+        Console.WriteLine($"The average is: {avernumber}");
+
+        int maxnumber = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > maxnumber)
+            {
+                maxnumber = number;
+
+            }
+        }
+        Console.WriteLine($"The largest number is: {maxnumber}");
     }
 }
