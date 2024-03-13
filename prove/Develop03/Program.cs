1e8e124577e515829fac22a8937ee98fa8b2cfcd
David Michael Innocent <ldsholder3@gmail.com>
2024-03-13 23:51:41 +0100

    updated

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,54 @@
 using System;
 
-class Program
+namespace MyScripture
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello Develop03 World!");
+        static void Main()
+        {
+            Console.WriteLine("Choose a scripture to memorize:");
+            Console.WriteLine("1. Proverbs 3:5-6");
+            Console.WriteLine("2. John 3:16");
+            Console.Write("Enter your choice (1 or 2): ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+            Scripture scripture;
+
+            if (choice == 1)
+            {
+                scripture = new Scripture("Proverb", 3, 5, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. 6: In all thy ways acknowledge him, and he shall direct thy paths.");
+            }
+            else if (choice == 2)
+            {
+                scripture = new Scripture("John", 3, 16, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life .");
+            }
+            else
+            {
+                Console.WriteLine("Invalid choice. Exiting program.");
+                return;
+            }
+
+            Console.Clear();
+            scripture.Display();
+            Console.WriteLine("\nPress Enter to continue or type 'quit' to end.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input.ToLower() == "quit")
+                    break;
+
+                if (!scripture.HideRandomWord())
+                {
+                    Console.WriteLine("\nCongrats, you got this Scripture memorization!");
+                    Console.WriteLine();
+                    break;
+                }
+
+                Console.Clear();
+                scripture.Display();
+                Console.WriteLine("\nPress Enter to continue or type 'quit' to end.");
+            }
+        }
     }
 }
