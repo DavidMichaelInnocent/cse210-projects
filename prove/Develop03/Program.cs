ee8bb9a4d54c4c7c150001f08f9b8b1ff813e507
David Michael Innocent <ldsholder3@gmail.com>
2024-03-15 18:33:12 +0100

    updaated

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,7 +6,8 @@ namespace MyScripture
     {
         static void Main()
         {
-            Console.WriteLine("Choose a scripture to memorize:");
+            Console.WriteLine();
+            Console.WriteLine(" Welcome to the scripture memorize! Choose your preffer scripture to memorize:");
             Console.WriteLine("1. Proverbs 3:5-6");
             Console.WriteLine("2. John 3:16");
             Console.Write("Enter your choice (1 or 2): ");
@@ -16,11 +17,11 @@ namespace MyScripture
 
             if (choice == 1)
             {
-                scripture = new Scripture("Proverb", 3, 5, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. 6: In all thy ways acknowledge him, and he shall direct thy paths.");
+                scripture = new Scripture("Proverb", 3, 5, "Trust in the Lord with all thine heart; and lean not unto thine own understanding. \n6: In all thy ways acknowledge him, and he shall direct thy paths.");
             }
             else if (choice == 2)
             {
-                scripture = new Scripture("John", 3, 16, "For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life .");
+                scripture = new Scripture("John", 3, 16, "`` For God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life. ` `");
             }
             else
             {
@@ -55,7 +56,7 @@ namespace MyScripture
 
 /*
 Show creativity and Exceeded requirement.
-1. Offering a Choice of Scriptures:
+1. I Offering a Choice of Scriptures:
  * The program lets the user choose from two scriptures to memorize, rather than just working with a single one.
 2. Allowing for Multiple Verses:
  * It can handle scriptures with multiple verses, such as "Proverbs 3:5-6", demonstrating flexibility in scripture selection.
